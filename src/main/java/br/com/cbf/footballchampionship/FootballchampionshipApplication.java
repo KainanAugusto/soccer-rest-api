f27d28a3e76133034ebcbf36f3da0fa702aa553b
Kainan Augusto <kbradachi@gmail.com>
2025-06-11 09:48:05 -0300

    Hello world

---
+++ src/main/java/br/com/cbf/footballchampionship/FootballchampionshipApplication.java
@@ -0,0 +1,13 @@
+package br.com.cbf.footballchampionship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballchampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballchampionshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cbf/footballchampionship/FootballchampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cbf.footballchampionship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballchampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
